931d1209b608d2e88400d46a28ebadbda9bef4c5
Luke Zappia <lazappi@users.noreply.github.com>
2021-03-24 16:19:47 +0100

    Add DatasetCardField component

--- src/components/DatasetCard.vue
+++ src/components/DatasetCard.vue
@@ -8,7 +8,7 @@
     >
       <p
         class="text-xs leading-6 font-medium text-gray-500 overflow-hidden"
-        v-bind:class="[open ? 'hidden' : '']"
+        :class="[open ? 'hidden' : '']"
       >
         {{ data.id }}
       </p>
@@ -18,134 +18,51 @@
       </p>
     </div>
     <div class="border-t border-gray-200">
-      <dl v-bind:class="[open ? '' : 'hidden']">
-        <div class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500">ID</dt>
-          <dd
-            class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3 break-words"
-          >
-            {{ data.id }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Author</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.author }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">DOI</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            <a
-              :href="'https://doi.org/' + data.doi"
-              class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
-              >{{ data.doi }}</a
-            >
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Year</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.year }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Number of cells</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.ncells }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Normalization</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.normalization }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Organism</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.organism }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Organ(s)</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.organ.join(', ') }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Age(s)</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.age.join(', ') }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Sex(es)</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.sex.join(', ') }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Healthy?</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.healthy }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">State</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.state }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Annotated?</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.annotated }}
-          </dd>
-        </div>
-        <div
-          class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
-        >
-          <dt class="text-sm font-medium text-gray-500">Cell types</dt>
-          <dd class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
-            {{ data.cell_types.join(', ') }}
-          </dd>
-        </div>
+      <dl :class="[open ? '' : 'hidden']">
+        <DatasetCardField name="ID" :value="data.id" />
+        <DatasetCardField name="Author" :value="data.author" />
+        <DatasetCardField
+          name="DOI"
+          :value="data.doi"
+          :link="'https://doi.org/' + data.doi"
+        />
+        <DatasetCardField name="Year" :value="data.year" />
+        <DatasetCardField
+          name="Number of cells"
+          :value="data.ncells.toString()"
+        />
+        <DatasetCardField name="Normalization" :value="data.normalization" />
+        <DatasetCardField name="Organism" :value="data.organism" />
+        <DatasetCardField name="Organ(s)" :value="data.organ.join(', ')" />
+        <DatasetCardField name="Age(s)" :value="data.age.join(', ')" />
+        <DatasetCardField name="Sex(es)" :value="data.sex.join(', ')" />
+        <DatasetCardField name="Healthy?" :value="data.healthy" />
+        <DatasetCardField name="State" :value="data.state" />
+        <DatasetCardField name="Annotated?" :value="data.annotated" />
+        <DatasetCardField
+          name="Cell types"
+          :value="data.cell_types.join(', ')"
+        />
       </dl>
     </div>
   </div>
 </template>
 
 <script>
+import DatasetCardField from './DatasetCardField.vue'
+
 export default {
   name: 'DatasetCard',
 
+  components: {
+    DatasetCardField
+  },
+
   props: {
-    data: Object
+    data: {
+      type: Object,
+      default: () => {}
+    }
   },
 
   data() {
---
+++ src/components/DatasetCardField.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    v-if="value !== ''"
+    class="odd:bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-6"
+  >
+    <dt class="text-sm font-medium text-gray-500">{{ name }}</dt>
+    <dd
+      v-if="link === ''"
+      class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3 break-words"
+    >
+      {{ value }}
+    </dd>
+    <dd v-else class="mt-1 mr-10 text-sm text-gray-900 sm:mt-0 sm:col-span-3">
+      <a
+        :href="link"
+        class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+        >{{ value }}</a
+      >
+    </dd>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DatasetCardField',
+
+  props: {
+    name: {
+      type: String,
+      default: () => 'Name'
+    },
+    value: {
+      type: String,
+      default: () => 'Value'
+    },
+    link: {
+      type: String,
+      default: () => ''
+    }
+  }
+}
+</script>
