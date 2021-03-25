604dfef7431d466fb55872f8795b155ec3f17f21
Luke Zappia <lazappi@users.noreply.github.com>
2021-03-25 10:20:39 +0100

    Add draft logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -65,13 +65,13 @@
           <div class="flex-shrink-0 flex items-center">
             <img
               class="block lg:hidden h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-              alt="Workflow"
+              src="../assets/icon.png"
+              alt="sfaira portal icon"
             />
             <img
               class="hidden lg:block h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-              alt="Workflow"
+              src="../assets/logo.png"
+              alt="sfaira portal logo"
             />
           </div>
           <!-- Page link buttons -->
