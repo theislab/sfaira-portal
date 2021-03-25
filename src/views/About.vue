56d4de1e078180f2eb43fbc685d75c31f532cfe9
Luke Zappia <lazappi@users.noreply.github.com>
2021-03-25 11:18:07 +0100

    Add some dummy content to Home and About views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <h1>This is an about page</h1>
+  <div class="w-full sm:w-2/3 mx-auto">
+    <p class="my-10 text-6xl text-center">This is the about page</p>
+    <p class="my-10 text-4xl text-center">
+      Here we can put more detailed information about sfaira, maybe FAQs, more
+      detailed explanations of things etc.
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <h1>This a home page</h1>
+  <div class="w-full sm:w-2/3 mx-auto">
+    <div class="m-auto my-10">
+      <img
+        class="lg:block w-full max-w-2xl m-auto"
+        src="../assets/logo.png"
+        alt="sfaira portal logo"
+      />
+    </div>
+    <p class="my-10 text-6xl text-center">Welcome to the sfaira portal!</p>
+    <p class="my-10 text-4xl text-center">
+      Here we should put a bit of an introduction and links to various pages,
+      GitHub repos etc.
+    </p>
   </div>
 </template>
